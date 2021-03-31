6ac587520a9b275f773cefb6d2d0d754abab20fd
Cathy Han <cathyhan@berkeley.edu>
2021-03-31 10:54:18 -0700

    fix jalr

--- tests/part-b/custom/inputs/jalr.s
+++ tests/part-b/custom/inputs/jalr.s
@@ -1,16 +1,20 @@
+jal ra, forward
 addi a0, x0, 1775
 addi a1, x0, -203
 sub a2, a0, a1
 addi a3, x0, 2000
-jalr x0, ra, forward
+jal ra, forward_again
+addi a1, x0, 2
+slti a1, a1, 5
+jal ra, end
 
-backward:
+forward_again:
 sub a2, a2, a3
-jalr x0, ra, end
+jalr x0, ra, 0
 
 forward:
 addi a4, x0, -1
-jalr x0, ra, backward
+jalr x0, ra, 0
 
 end:
 addi a0, x0, -100
